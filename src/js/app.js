724431ce03a1f0b88d09730124c42e135028d7ed
maluy <artem.evsiukov1@gmail.com>
2022-04-19 15:22:28 +0300

    add slider

--- src/js/app.js
+++ src/js/app.js
@@ -2,13 +2,16 @@
 //
 // This file will be compiled into app.js and will not be minified.
 // Feel free with using ES6 here.
-
-//import Accordion from './components/accordion';
+import burgerMenu from './components/burger-menu';
+import swiperSlider from './components/slider';
+import tabs from './components/tabs';
 
 (($) => {
   // When DOM is ready
   $(() => {
     //const accordions = new Accordion();
-    //burgerMenu.init();
+    burgerMenu.init();
+    swiperSlider.init();
+    tabs.init();
   });
 })(jQuery);
--- src/js/components/slider.js
+++ src/js/components/slider.js
@@ -1,28 +1,56 @@
+import Swiper from 'swiper/bundle';
 const swiperSlider = (() => {
-  const accountantInit = new Swiper('.js-accountant-slider', {
-    slidesPerView: 2,
+  const accountantInit = new Swiper('.js-benefit-slider', {
     spaceBetween: 20,
+    slidesPerView: 1,
     speed: 800,
-    centerInsufficientSlides: true,
-    pagination: {
-      el: '.swiper-pagination',
-      type: 'bullets',
-      clickable: true,
+    centeredSlides: false,
+    loop: true,
+    freemod: true,
+
+    navigation: {
+      nextEl: '.benefit-button-next',
+      prevEl: '.benefit-button-prev',
     },
     breakpoints: {
+      540: {
+        spaceBetween: 20,
+        slidesPerView: 2,
+      },
       768: {
-        spaceBetween: 40,
+        spaceBetween: 30,
         slidesPerView: 3,
+      },
+      1200: {
+        spaceBetween: 31,
+        slidesPerView: 4,
+      },
+      1360: {
+        spaceBetween: 31,
+        slidesPerView: 6,
       },
     },
   });
+
+//   const benefitSwiper = new Swiper('.js-benefit-slider-two', {
+//     spaceBetween: 20,
+//     slidesPerView: 1,
+//     speed: 800,
+//     centeredSlides: false,
+//     loop: true,
+//     freemod: true,
+
+//     navigation: {
+//       nextEl: '.benefit-button-next',
+//       prevEl: '.benefit-button-prev',
+//     },
+//   });
 
   const init = () => {};
 
   return {
     init,
   };
-
 })();
 
 export default swiperSlider;
